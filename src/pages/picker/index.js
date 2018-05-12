b0836fca80c7c4e84b4a4260f9746505e7fa759e
zhujincheng <452335710@qq.com>
2018-05-12 18:40:56 +0800

    linshi

---
+++ src/pages/picker/index.js
@@ -0,0 +1,12 @@
+import initAreaPicker, { getSelectedAreaData } from '../../template/index';
+
+Page({
+  onShow: () => {
+    initAreaPicker({
+      hideDistrict: true, // 是否隐藏区县选择栏，默认显示
+    });
+  },
+  getSelecedData() {
+    console.table(getSelectedAreaData());
+  }
+});
